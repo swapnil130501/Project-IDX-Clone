3fb178fc61b4d585205cba4c07ad7e68b9fcaeb6
swapnil <swapnil130501@gmail.com>
2024-12-06 09:12:07 +0530

    adding new project using exec node module

--- backend/src/routes/v1/index.js
+++ backend/src/routes/v1/index.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import { pingCheck } from '../../controllers/pingController.js';
+import ProjectRouter from './projects.js';
 
 const router = express.Router();
 router.use('/ping', pingCheck);
+router.use('/projects', ProjectRouter);
 
 export default router;
