02a272166b5895ff9171a0332d17b99c2bbb7111
swapnil <swapnil130501@gmail.com>
2024-12-11 10:27:24 +0530

    setting up websockets using socket.io

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,16 +1,29 @@
 import express from 'express';
 import { PORT } from './config/serverConfig.js';
 import cors from 'cors';
+import { createServer } from 'node:http';
+import { Server } from 'socket.io';
 import apiRouter from '../src/routes/index.js';
 
 const app = express();
+const server = createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: '*',
+        method: ['GET', 'POST']
+    }
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
+io.on('connection', (socket) => {
+    console.log('a user connected')
+});
+
 app.use('/api', apiRouter);
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`server started on port ${PORT}`)
 })
