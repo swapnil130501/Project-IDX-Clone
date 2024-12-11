42b67770912c31baa4d00added6c54dc5991e30c
swapnil <swapnil130501@gmail.com>
2024-12-11 10:28:04 +0530

    setting client socket connection

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,10 @@
 import './App.css'
 import { Router } from './Router.jsx'
+import { io } from 'socket.io-client'
 
 function App() {
-    
+    const socket = io("http://localhost:3000");
+
     return ( 
         <>
             <Router />
