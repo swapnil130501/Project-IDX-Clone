ad128d67a820541e5606b12f0d495d5be2099149
swapnil <swapnil130501@gmail.com>
2024-12-12 10:16:27 +0530

    add editor socket setup for reading file

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,8 @@ import cors from 'cors';
 import { createServer } from 'node:http';
 import { Server } from 'socket.io';
 import apiRouter from '../src/routes/index.js';
+import chokidar from 'chokidar';
+import { handleEditorSocketEvents } from './socketHandlers/editorHandler.js';
 
 const app = express();
 const server = createServer(app);
@@ -23,6 +25,37 @@ io.on('connection', (socket) => {
 });
 
 app.use('/api', apiRouter);
+
+const editorNamespace = io.of('/editor');
+editorNamespace.on('connection', (socket) => {
+    console.log('editor connected');
+
+    let projectId = socket.handshake.query['projectId'];
+
+    console.log('projectId received afer connection', projectId);
+
+    if(projectId) {
+        var watcher = chokidar.watch(`./projects/${projectId}`, {
+            ignored: (path) => path.includes("node_modules"),
+            persistent: true,
+            awaitWriteFinish : {
+                stabilityThreshold: 2000,
+            },
+            ignoreInitial: true,
+        });
+
+        watcher.on("all", (event, path) => {
+            console.log(event, path);
+        })
+    }
+
+    handleEditorSocketEvents(socket);
+
+    socket.on('disconnect', async () => {
+        await watcher.close();
+        console.log('editor disconnected');
+    });
+});
 
 server.listen(PORT, () => {
     console.log(`server started on port ${PORT}`)
