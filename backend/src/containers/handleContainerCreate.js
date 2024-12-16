69cd6179b3b1e0bd4c6e24672f65aee9101f77f1
swapnil <swapnil130501@gmail.com>
2024-12-16 10:21:51 +0530

    stream config for data writing on terminal

--- backend/src/containers/handleContainerCreate.js
+++ backend/src/containers/handleContainerCreate.js
@@ -35,8 +35,53 @@ export const handleContainerCreate = async(projectId, socket) => {
         console.log("container created", container.id);
         await container.start();
         console.log('container started successfully');
-        
+
+        container.exec({
+            Cmd: ['/bin/bash'],
+            User: 'sandbox',
+            AttachStdin: true,
+            AttachStdout: true,
+            AttachStderr: true,
+        }, (err, exec) => {
+            if(err) {
+                console.log('error while creating exec', err);
+                return;
+            }
+
+            exec.start({ hijack: true }, (err, stream) => {
+                if(err) {
+                    console.log('error while starting exec', err);
+                    return;
+                }
+
+                processStream(stream, socket);
+
+                socket.on('shell-input', (data) => {
+                    stream.write('ls\n');
+                });
+            })
+        })
+
     } catch (error) {
         console.log('error while creating container', error)
     }
+}
+
+function processStream(stream, socket) {
+    let buffer = Buffer.from('');
+    stream.on('data', (data) => {
+        buffer = Buffer.concat([buffer, data]);
+        socket.emit('shell-output', buffer.toString());
+        buffer = Buffer.from('');
+    });
+
+    stream.on('end', () => {
+        console.log('stream ended');
+        socket.emit('shell-output', 'stream ended');
+    });
+
+    stream.on('error', () => {
+        console.log('stream closed', err);
+        socket.emit('shell-output', 'stream err');
+    });
 }
--- backend/src/index.js
+++ backend/src/index.js
@@ -64,10 +64,10 @@ terminalNamespace.on('connection', (socket) => {
 
     let projectId = socket.handshake.query['projectId'];
 
-    socket.on('shell-input', (data) => {
-        console.log('input received', data);
-        terminalNamespace.emit('shell-output', data);
-    });
+    // socket.on('shell-input', (data) => {
+    //     console.log('input received', data);
+    //     terminalNamespace.emit('shell-output', data);
+    // });
 
     socket.on('disconnect', () => {
         console.log('terminal disconnected');
