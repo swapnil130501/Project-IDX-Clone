25701c1aa8b1711adc69f3cde2a6b6ee0ca83ba3
swapnil <swapnil130501@gmail.com>
2024-12-21 21:42:24 +0530

    add get port socket and terminalApp

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,9 +6,6 @@ import apiRouter from './routes/index.js';
 import { PORT } from './config/serverConfig.js';
 import chokidar from 'chokidar';
 import { handleEditorSocketEvents } from './socketHandlers/editorHandler.js';
-import { WebSocketServer } from 'ws';
-import { handleTerminalCreation } from './containers/handleTerminalCreation.js';
-import { handleContainerCreate, listContainer } from './containers/handleContainerCreate.js';
 
 const app = express();
 const server = createServer(app);
@@ -53,11 +50,6 @@ editorNamespace.on("connection", (socket) => {
             console.log(event, path);
         });
     }
-   
-    socket.on("getPort", () => {
-        console.log("getPort event received");
-        listContainer();
-    })
 
     handleEditorSocketEvents(socket, editorNamespace);
 
@@ -66,46 +58,4 @@ editorNamespace.on("connection", (socket) => {
 server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
     console.log(process.cwd())
-});
-
-const webSocketForTerminal = new WebSocketServer({
-    noServer: true // we will handle the upgrade event
-});
-
-webSocketForTerminal.on("connection", (ws, req, container) => {
-    console.log("Terminal connected",container);
-    handleTerminalCreation(container, ws);
-
-    // ws.on("getPort", () => {
-    //     console.log("getPort event received");
-    // })
-
-    ws.on("close", () => {
-        container.remove({ force: true}, (err, data) => {
-            if(err) {
-                console.log("Error while removing container", err);
-            }
-            console.log("Container removed", data);
-        });
-    })
-});
-
-
-server.on("upgrade", (req, tcp, head) => {
-    /**
-     * req: Incoming http request
-     * socket: TCP socket
-     * head: Buffer containing the first packet of the upgraded stream
-     */
-    // This callback will be called when a client tries to connect to the server through websocket
-
-    const isTerminal = req.url.includes("/terminal");
-
-    if(isTerminal) {
-        console.log("req url received", req.url);
-        const projectId = req.url.split("=")[1];
-        console.log("Project id received after connection", projectId);
-
-        handleContainerCreate(projectId, webSocketForTerminal, req, tcp, head);
-    }
 });
--- backend/src/socketHandlers/editorHandler.js
+++ backend/src/socketHandlers/editorHandler.js
@@ -1,4 +1,5 @@
 import fs from 'fs/promises';
+import { getContainerPort } from '../containers/handleContainerCreate.js';
 
 export const handleEditorSocketEvents = (socket, editorNamespace) => {
     socket.on('writeFile', async ({ data, pathToFileOrFolder }) => {
@@ -109,4 +110,12 @@ export const handleEditorSocketEvents = (socket, editorNamespace) => {
             })
         }
     });
+
+    socket.on("getPort", async ({ containerName }) => {
+        const port = await getContainerPort(containerName);
+        console.log("port data", port);
+        socket.emit("getPortSuccess", {
+            port: port,
+        })
+    })
 }
